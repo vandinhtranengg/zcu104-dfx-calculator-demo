d670ed658a30aad375d65a4176ef204ae4aa468e
vandinhtranengg <135353645+vandinhtranengg@users.noreply.github.com>
2026-05-21 10:48:56 +0900

    Add files via upload

---
+++ hls/rp_calculator_add/rp_calculator_add.cpp
@@ -0,0 +1,17 @@
+#include <ap_int.h>
+
+void rp_calculator_add(
+    ap_uint<32> a,
+    ap_uint<32> b,
+    ap_uint<32> *result,
+    ap_uint<32> *module_id
+) {
+#pragma HLS INTERFACE s_axilite port=a         bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=b         bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=result    bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=module_id bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=return    bundle=CTRL
+
+    *result = a + b;
+    *module_id = 0x00000001;
+}
---
+++ hls/rp_calculator_add/tb_rp_calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ap_int.h>
+
+void rp_calculator_add(
+    ap_uint<32> a,
+    ap_uint<32> b,
+    ap_uint<32> *result,
+    ap_uint<32> *module_id
+);
+
+int main() {
+    ap_uint<32> a = 20;
+    ap_uint<32> b = 7;
+    ap_uint<32> result = 0;
+    ap_uint<32> module_id = 0;
+
+    rp_calculator_add(a, b, &result, &module_id);
+
+    std::cout << "A         = " << a << std::endl;
+    std::cout << "B         = " << b << std::endl;
+    std::cout << "RESULT    = " << result << std::endl;
+    std::cout << "MODULE ID = " << std::hex << module_id << std::endl;
+
+    return 0;
+}
